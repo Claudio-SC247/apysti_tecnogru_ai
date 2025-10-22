27b3c6f158c5710b292c7de51c950b22a1354fd4
Claudio <claudiocrispin982@gmail.com>
2025-10-22 00:10:06 -0500

    descripcion

---
+++ app-b.py
@@ -0,0 +1,5 @@
+"""
+Elabore una aplicación que permita simular el uso de pandas para visualizar algunas estadísticas (ud. 
+defina lo que desea medir para visualizarlas) con respecto a una encuesta, relacionada a preferencias de 
+supermercados en Piura.
+"""
